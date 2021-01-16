af5dd5f5a87d6087c7115f1e5d36e2a15a09a5b7
Clarence <cl.sheng34@gmail.com>
2021-01-16 17:53:37 +0800

    added functionality to post image

--- frontend/src/pages/captureScreen/captureScreen.jsx
+++ frontend/src/pages/captureScreen/captureScreen.jsx
@@ -6,8 +6,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCamera } from '@fortawesome/free-solid-svg-icons'
 
 const CaptureScreen = () => {
-    const imageCanvas = useRef(null)
-    // const [hasVideoAccess,]
+    const fileReader = useRef(null)
+    const [hasVideoAccess, setVideoAccess] = useState(false)
+    const [isImageTaken, setImageTaken] = useState(false)
+    const [picture, setPicture] = useState('')
 
     useEffect(()=>{
         if (!('mediaDevices' in navigator)){
@@ -31,25 +33,75 @@ const CaptureScreen = () => {
             video: true,
             audio: false
         }).then(stream => {
+            setVideoAccess(true)
             let video = document.querySelector(".fridge-capture")
             video.srcObject = stream
             video.addEventListener('loadedmetadata', () => {
                 video.play()
             })
-            console.log("success")
+            
         })
     },[])
 
+    const takePicture = _ => {
+        setImageTaken(true)
+        let canvas = document.querySelector(".image-canvas")
+        let video = document.querySelector(".fridge-capture")
+        let context = canvas.getContext('2d')
+        context.drawImage(video, 0, 0, canvas.width, video.videoHeight/ (video.videoWidth/canvas.width))
+        video.srcObject.getVideoTracks().forEach(track => {
+            track.stop()
+        })
+        setVideoAccess(false)
+        setPicture(dataURItoBlob(canvas.toDataURL()))
+    }
+
+    const sendImage = () => { 
+        let id = new Date().toISOString()
+        const formData = new FormData()
+        formData.append("image", picture, id + '.png')
+        fetch('url', {
+            method: 'POST',
+            body: formData
+        })
+    }
+
+    useEffect(_ => console.log(picture), [picture])
+
+    const dataURItoBlob = dataURI => {
+        var byteString = atob(dataURI.split(',')[1]);
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+        var ab = new ArrayBuffer(byteString.length);
+        var ia = new Uint8Array(ab);
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+        var blob = new Blob([ab], {type: mimeString});
+        return blob; 
+    }
+
+    const convertFileToBlob = file => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.addEventListener("load", function () {
+            setPicture(dataURItoBlob(reader.result));
+          }, false);
+    }
+
     return(
-        <div className="capture-screen">
-            <video className="fridge-capture"/>
-            <canvas ref={imageCanvas} className="image-canvas"/>
-            <button className="capture-btn">
+        <div className="capture-screen page">
+            <div>Take a picture of your fridge :D</div>
+            {hasVideoAccess ? <video className="fridge-capture"/> : ''}
+            <canvas className={`image-canvas ${isImageTaken ? "": "hidden"}`} width="320px" height="240px"/>
+            <button className="capture-btn" onClick={takePicture} disabled={isImageTaken ? true:false}>
                 <FontAwesomeIcon icon={faCamera}/>
             </button>
             <div className="image-picker">
-                <input className="image-select" type="file" accept="image/*"></input>
+                <input ref={fileReader} onChange={e=>{convertFileToBlob(fileReader.current.files[0])}} className="image-select" type="file" accept="image/*"></input>
             </div>
+            <button onClick={sendImage}> 
+                Get Ingredients
+            </button>
         </div>
     )
 }
