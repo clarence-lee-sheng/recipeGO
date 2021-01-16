9de31c19b64fdba639e6a92ef6f76b55983654ff
Ryan Hu <43288994+Ryanh8@users.noreply.github.com>
2021-01-16 01:47:31 -0500

    Add files via upload

---
+++ backend/vision.py
@@ -0,0 +1,19 @@
+from google.cloud import vision
+import io
+
+client = vision.ImageAnnotatorClient()
+
+# Loads the image into memory
+with io.open(r"e:/downloads/test.jpg", 'rb') as image_file:
+    content = image_file.read()
+
+image = vision.Image(content=content)
+
+# Performs label detection on the image file
+response = client.label_detection(image=image)
+print(type(response))
+labels = response.label_annotations
+
+print('Labels:')
+for label in labels:
+    print(label.description)
